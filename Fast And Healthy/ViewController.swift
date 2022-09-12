00c8a45bd9bec07a816d08448a1e0defa7fe71e0
Joshua Joseph <jjneymarcr7@gmail.com>
2022-09-11 20:48:36 -0700

    Initial Commit

---
+++ Fast And Healthy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fast And Healthy
+//
+//  Created by Joshua on 9/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Fast And HealthyUITests/Fast_And_HealthyUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Fast_And_HealthyUITestsLaunchTests.swift
+//  Fast And HealthyUITests
+//
+//  Created by Joshua on 9/11/22.
+//
+
+import XCTest
+
+class Fast_And_HealthyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
